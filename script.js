e57290a1176b27a18493781c9be512e9ab84294e
Joshua Alonge <alongejoshua@gmail.com>
2022-11-02 18:45:40 +0100

    creation of grids within the container

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const container = document.querySelector('.container');
 
 // creating 16x16 DIVs
-for (let i = 0; i < (16*16); i++){
+for (let i = 0; i < 50; i++){
     // create a div that with display: flex or
+    const line = document.createElement('div');
+    line.style.cssText = "display:flex;"
     // use flex wrap on the container
-    //for (let j = 0; j < 16; j++){ 
-    const box = document.createElement('div');
-    box.className = "box";    
+    for (let j = 0; j < 50; j++){ 
+        const box = document.createElement('div');
+        box.className = "box";
+        box.style.cssText = "margin=0;width:8px; height:8px;border: 0.5px solid grey;"
+        line.appendChild(box)
     //divide the length of the container to get each div size
     //create and insert each div in the container
-    container.appendChild(box)
+    container.appendChild(line)
 }
-box.style.cssText = "width:(500/16); height:(500/16); border: 1px solid black"
-
-    // add the parent div to the container class
+}    
+// add the parent div to the container class
