6d6c135750b16ff4aae1210f4cd6e590c7e07eed
Alien <alongejoshua@gmail.com>
2023-08-20 17:36:42 +0100

    creating the pixlated design but incomplete

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Etch-A-Sketch
+
+`A webpage with a Grid of square divs.`
+
+- Creates the divs using JavaScript.
+- Put your grid squares inside another “container” div (which can go directly in your HTML).
+- Make the divs appear as a grid (versus just one on each line). 
+- This is a perfect opportunity to apply flexbox.
+- Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+- There are multiple ways to change the color of the divs, including:
+    - adding a new class to the div.
+    - changing the div’s background color using JavaScript.
+
+- Adds a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid.
+
--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const container = document.querySelector('.container');
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector('.container');
+    const input = document.querySelector('#pixel_range');
+    const divNum = input.value;
 
-// creating 16x16 DIVs
-for (let i = 0; i < 50; i++){
-    // create a div that with display: flex or
-    const line = document.createElement('div');
-    line.style.cssText = "display:flex;"
-    // use flex wrap on the container
-    for (let j = 0; j < 50; j++){ 
+    const isMouseDown = false;
+
+    
+    if (divNum != input.value) {
+        input.addEventListener("input", (event) => {
+            const newValue = event.target.value;
+            console.log(newValue)
+
+        });
+    }
+    // addEventListener incase there is changes in value
+        
+    console.log(divNum);
+
+    // creating any numXnum DIVs using flex display
+    for (let i = 0; i < (divNum * divNum); i++){
         const box = document.createElement('div');
         box.className = "box";
-        box.style.cssText = "margin=0;width:8px; height:8px;border: 0.5px solid grey;"
-        line.appendChild(box)
-    //divide the length of the container to get each div size
-    //create and insert each div in the container
-    container.appendChild(line)
-}
-}    
-// add the parent div to the container class
+        const num = 100 / divNum;
+        const widthNum = Math.round(num * 1000) / 1000;
+        
+        box.style.flexBasis = `calc(${widthNum}% - 2px)`;
+        // if mouse goes over div change color
+        box.addEventListener("mouseover", () => {
+            box.classList.add('hovered');
+        });
+        container.appendChild(box);
+    }
+
+    
+});    
+
